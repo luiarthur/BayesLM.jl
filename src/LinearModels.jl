1e8f1295a0a3b54c997ffad20f44c17d5494b16e
Arthur Lui <luiarthur@gmail.com>
2016-10-26 11:35:25 -0700

    MCMC.jl module became internal

--- src/LinearModels.jl
+++ src/LinearModels.jl
@@ -1,12 +1,12 @@
 __precompile__()
 module LinearModels
 
-import MCMC
 import Base.show
 using Distributions
 
 export lm, post_summary, dic, show
 
+include("MCMC.jl")
 include("common.jl")
 include("lm.jl")
 
